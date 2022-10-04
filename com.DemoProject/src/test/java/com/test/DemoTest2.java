26662523144900cbdbd879227f8a5a9a7a021ff7
shivk <shivk@shivkumarK>
2022-10-04 23:24:54 +0530

    adding a new class

---
+++ com.DemoProject/src/test/java/com/test/DemoTest2.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+	
+	@Test
+	public void test01() {
+		System.out.println("Hello World");
+	}
+
+	@Test
+	public void test02() {
+		System.out.println("Hello World");
+ }
+}
